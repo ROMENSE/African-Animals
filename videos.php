51fdbd3b88a08bf81788c95513930dda371d59e2
ROMENSE <dcm.mohammed85@gmail.com>
2016-05-05 23:32:58 +0200

    Added files via upload

--- videos.php
+++ videos.php
@@ -2,45 +2,49 @@
 include('usheader.php');
 ?>
 <br><br><br><br><br>
-<div class="container" class="  watch        video-MQ5ljhweCU4 watch-non-stage-mode">
 
- <div class="row">
-    <div align="center" class="col-sm-24">
-<div align="center" class="col-sm-12">
 
-<video width="560" height="440" controls>
-  <source src="videos/lion.mp4" type="video/mp4">
-  <source src="movie.ogg" type="video/ogg">
- 
-</video>
+
+<div align="right" class="col-sm-12">
+<div align="right" class="col-sm-4">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/aiXbwl2RBhc" frameborder="0" allowfullscreen></iframe>
 </div>
-<div align="center" class="col-sm-12">
-<video width="560" height="440" controls>
-  <source src="videos/2.mp4" type="video/mp4">
-  <source src="movie.ogg" type="video/ogg">
- 
-</video>
+
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/ss7EJ-PW2Uk" frameborder="0" allowfullscreen></iframe>
+<br><br><br>
+   
+ <div align="right" class="col-sm-4">
+<iframe width="420" height="315" src="https://www.youtube.com/embed/09pna-4PTJk" frameborder="0" allowfullscreen></iframe>
 </div>
-    <div align="center" class="col-sm-12">
- 
-<video width="560" height="440" controls>
-  <source src="videos/topi.mp4" type="video/mp4">
-  <source src="movie.ogg" type="video/ogg">
- 
-</video>
+
+
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/0I9-NJZ8Vxk" frameborder="0" allowfullscreen></iframe>
+<br><br>
+<div align="right" class="col-sm-4">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/aiXbwl2RBhc" frameborder="0" allowfullscreen></iframe>
 </div>
-<div align="center" class="col-sm-12">
-<video width="560" height="440" controls>
-  <source src="videos/4.mp4" type="video/mp4">
-  <source src="movie.ogg" type="video/ogg">
- 
-</video>
+
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/ss7EJ-PW2Uk" frameborder="0" allowfullscreen></iframe>
+<br><br><br>
+   
+ <div align="right" class="col-sm-4">
+<iframe width="420" height="315" src="https://www.youtube.com/embed/09pna-4PTJk" frameborder="0" allowfullscreen></iframe>
 </div>
+
+
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/0I9-NJZ8Vxk" frameborder="0" allowfullscreen></iframe>
+<br><br><br><br>
+
 </div>
+
   
 
 
-</div>
+
 <br><br><br><br><br>
 <?php
 include('footer.php');
